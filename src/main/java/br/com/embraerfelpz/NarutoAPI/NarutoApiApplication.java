a8b8736e8eafe686af49993ef600d765392d7a37
Felipe <fcardoso@globmail.com.br>
2025-11-02 21:10:30 -0300

    First

---
+++ src/main/java/br/com/embraerfelpz/NarutoAPI/NarutoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.embraerfelpz.NarutoAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarutoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NarutoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/embraerfelpz/NarutoAPI/NarutoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.embraerfelpz.NarutoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NarutoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
