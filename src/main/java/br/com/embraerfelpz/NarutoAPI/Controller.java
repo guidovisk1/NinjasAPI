62f06154747a896f179d512b28f88251694e1a23
Felipe <fcardoso@globmail.com.br>
2025-11-02 22:01:58 -0300

    Primeira rota mudando rota

--- src/main/java/br/com/embraerfelpz/NarutoAPI/Controller.java
+++ src/main/java/br/com/embraerfelpz/NarutoAPI/Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class Controller {
 
-    @GetMapping("/")
+    @GetMapping("/bemvindo")
     public String boasVindas() {
         System.out.println("Log");
         return "Boas vindas";
