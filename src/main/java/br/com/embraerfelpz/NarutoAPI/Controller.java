b9427af7dd024773e572441ed6e61f694e3c0657
Felipe <fcardoso@globmail.com.br>
2025-11-02 21:32:53 -0300

    Primeira rota

---
+++ src/main/java/br/com/embraerfelpz/NarutoAPI/Controller.java
@@ -0,0 +1,16 @@
+package br.com.embraerfelpz.NarutoAPI;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Use this anottations for Controllers
+@RequestMapping
+
+public class Controller {
+
+    @GetMapping("/")
+    public String boasVindas() {
+        System.out.println("Log");
+        return "Boas vindas";
+    }
+}
